7a81f25567357abcbc547ee13639a68f77aa8190
Nic19907 <88468884+Nic19907@users.noreply.github.com>
2022-06-02 20:51:34 -0600

    final

--- Server/Server.ino
+++ Server/Server.ino
@@ -21,6 +21,17 @@ String header;
 
 
 // Auxiliar variables to store the current output state
+#define RXD2 16
+#define TXD2 17
+
+// Del display
+#define e 18
+#define d 19
+#define c 21
+#define g 14
+#define f 27
+#define a 26
+#define b 25
 
 
 // Variables
@@ -38,12 +49,15 @@ struct Estados{
 
 unsigned char letra;
 
+char contador = 0;
 
+unsigned char steps = 0;
+unsigned char steps2 = 0;
 void setup() 
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
   // Connect to your wi-fi modem
   WiFi.begin(ssid, password);
 
@@ -62,6 +76,14 @@ void setup()
   server.begin();
   Serial.println("HTTP server started");
   delay(100);
+
+  pinMode(a, OUTPUT);  //A
+  pinMode(b, OUTPUT);  //B
+  pinMode(c, OUTPUT);  //C
+  pinMode(d, OUTPUT);  //D
+  pinMode(e, OUTPUT);  //E
+  pinMode(f, OUTPUT);  //F
+  pinMode(g, OUTPUT);  //G
 }
 
 void loop() 
@@ -74,6 +96,193 @@ void loop()
     //Serial.write(estado_parqueo.par0);
   }
   server.handleClient();
+
+  if (Serial.available()){
+    String CadenaEntrante = Serial.readStringUntil(',');
+    //Serial.println(CadenaEntrante);
+
+    String Parqueo00 = CadenaEntrante.substring(0,1);
+    String Parqueo01 = CadenaEntrante.substring(1,2);
+    String Parqueo02 = CadenaEntrante.substring(2,3);
+    String Parqueo03 = CadenaEntrante.substring(3,4);
+     estado_parqueo.par4 = CeroA1(Parqueo00.toInt());
+estado_parqueo.par5 = CeroA1(Parqueo00.toInt());
+estado_parqueo.par6 = CeroA1(Parqueo00.toInt());
+     estado_parqueo.par7 = CeroA1(Parqueo00.toInt());
+//     switch (steps2){
+//      case 0:
+//        estado_parqueo.par4 = CeroA1(Parqueo00.toInt());
+//        steps2= 1;
+//        break;
+//  
+//      case 1:
+//        estado_parqueo.par5 = CeroA1(Parqueo01.toInt());
+//        steps2 = 2;
+//        break;
+//  
+//      case 2:
+//        estado_parqueo.par6= CeroA1(Parqueo02.toInt());
+//        steps2 = 3;
+//        break;
+//  
+//      case 3:
+//        estado_parqueo.par7 = CeroA1(Parqueo03.toInt());
+//        steps2 = 0;
+//        break;
+//    }
+
+  }
+
+  if (Serial2.available()){
+    String CadenaEntrante = Serial2.readStringUntil(',');
+    Serial.println(CadenaEntrante);
+    //Serial.println(CadenaEntrante);
+
+    
+    String Parqueo0 = CadenaEntrante.substring(0,1);
+    String Parqueo1 = CadenaEntrante.substring(1,2);
+    String Parqueo2 = CadenaEntrante.substring(2,3);
+    String Parqueo3 = CadenaEntrante.substring(3,4);
+
+    estado_parqueo.par0 = CeroA1(Parqueo0.toInt());
+    estado_parqueo.par1 = CeroA1(Parqueo0.toInt());
+    estado_parqueo.par2 = CeroA1(Parqueo0.toInt());
+    estado_parqueo.par3 = CeroA1(Parqueo0.toInt());
+
+//    switch (steps){
+//      case 0:
+//        estado_parqueo.par0 = CeroA1(Parqueo0.toInt());
+//        steps = 1;
+//        break;
+//  
+//      case 1:
+//        estado_parqueo.par1 = CeroA1(Parqueo1.toInt());
+//        steps = 2;
+//        break;
+//  
+//      case 2:
+//        estado_parqueo.par2= CeroA1(Parqueo2.toInt());
+//        steps = 3;
+//        break;
+//  
+//      case 3:
+//        estado_parqueo.par3 = CeroA1(Parqueo3.toInt());
+//        steps = 0;
+//        break;
+//    }
+  }
+  counter7 ();
+  display7SEG (contador);
+  
+}
+
+void display7SEG (int digit){
+  switch (digit){
+    case 0:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, LOW);
+      break;
+
+    case 1:
+      digitalWrite(a, LOW);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW);
+      break;
+
+    case 2:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, LOW);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, LOW);
+      digitalWrite(g, HIGH);
+      break;
+
+    case 3:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, LOW);
+      digitalWrite(f, LOW);
+      digitalWrite(g, HIGH);
+      break;
+
+    case 4:
+      digitalWrite(a, LOW);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+
+    case 5:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, LOW);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, LOW);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+
+    case 6:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, LOW);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+
+    case 7:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW);
+      break;
+
+    case 8:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+
+    case 9:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+  }
+}
+
+void counter7 (){
+  contador = estado_parqueo.par0 + estado_parqueo.par1 + estado_parqueo.par2 + 
+  estado_parqueo.par4 + estado_parqueo.par5 + estado_parqueo.par6 + estado_parqueo.par7;
 }
 
 void handle_OnConnect()
@@ -86,7 +295,7 @@ String SendHTML()
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<html lang=\"en\">\n";
   ptr += "<head>\n";
-  ptr += "<script>function autoRefresh(){window.location=window.location.href}setInterval(\"autoRefresh()\",5000);</script>";
+  ptr += "<script>function autoRefresh(){window.location=window.location.href}setInterval(\"autoRefresh()\",1000);</script>";
   ptr += "<meta charset=\"UTF-8\">\n";
   ptr += "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n";
   ptr += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
@@ -204,4 +413,14 @@ String SendHTML()
   ptr += "</body>\n";
   ptr += "</html>";
   return ptr;
+}
+int CeroA1( int val){
+  switch(val){
+    case 0:
+      return 1;
+      break;
+    case 1:
+      return 0;
+      break;
+  }
 }
